6849341ef2d763265fd9238e41ce5098feb6cf4f
shahzod <yuldashallaberganov889@gmail.com>
2022-05-25 18:55:22 +0500

    starting project

---
+++ src/Routers.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import HomePage from "./pages/HomePage";
+
+function Routers() {
+  return (
+    <Fragment>
+      <HomePage />
+    </Fragment>
+  );
+}
+
+export default Routers;
