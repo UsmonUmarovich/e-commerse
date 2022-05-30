b0d81c181720a3eab8063a0d0226047df0f7dd2f
shahzod <yuldashallaberganov889@gmail.com>
2022-05-30 17:29:32 +0500

    api.service & AllProducts

---
+++ src/contexts/AllProducts.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import query from "../services/api/api.service";
+
+const AllProductsContext = createContext({
+  isLoading: false,
+  isError: false,
+  products: [],
+});
+AllProductsContext.displayName = "AllProductsContext";
+
+export const useAllProducts = () => useContext(AllProductsContext);
+
+const AllProductProvider = ({ children }) => {
+  const [allProducts, setAllProducts] = useState({
+    isLoading: false,
+    isError: false,
+    products: [],
+  });
+
+  useEffect(() => {
+    query
+      .getAllProducts()
+      .then((result) =>
+        setAllProducts({ isLoading: false, products: result, isError: false })
+      )
+      .catch((err) =>
+        setAllProducts({ isLoading: false, products: [], isError: true })
+      );
+  }, []);
+
+  return (
+    <AllProductsContext.Provider value={allProducts}>
+      {children}
+    </AllProductsContext.Provider>
+  );
+};
