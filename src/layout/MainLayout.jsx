dcaf62109a3f8a68b0bfb96e2633763a5faeaa92
shahzod <yuldashallaberganov889@gmail.com>
2022-05-30 18:01:57 +0500

    api.service & AllProduct

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
 import { useAllProducts } from "../contexts/AllProducts";
-import query from "../services/api/api.service";
 
 export const MainLayout = () => {
   const {isError, isLoading, products} = useAllProducts()
