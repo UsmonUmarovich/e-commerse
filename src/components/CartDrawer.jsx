636ad97118f0a183a388af486ad6e1d503f37e46
shahzod <yuldashallaberganov889@gmail.com>
2022-06-13 18:34:25 +0500

    shuffle.js created & CartDrawer & TotalSum changed

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -1,5 +1,12 @@
 import { Close } from "@mui/icons-material";
-import { Box, Button, Drawer, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useCart } from "../contexts/Cart";
 import { useDrawer } from "../contexts/Drawer";
@@ -10,56 +17,78 @@ export const CartDrawer = () => {
   const { open, closeDrawer } = useDrawer();
   const { cart, clearCart } = useCart();
   return (
-    <Drawer anchor="right" open={open} onClose={closeDrawer}>
-      <Box sx={{ width: 300, p: 2 }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row-reverse",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Typography sx={{ fontSize: 20 }}>Your Shopping Cart</Typography>
-
-          <IconButton>
-            <Close onClick={closeDrawer} />
-          </IconButton>
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            gap: 1,
-            mt: 2,
-          }}
-        >
-          <Button
-            onClick={clearCart}
-            sx={{ width: "100%" }}
-            color="warning"
-            variant="outlined"
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={closeDrawer}
+      sx={{
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          position: "sticky",
+          top: 0,
+          background: "#fff",
+          zIndex: 777,
+          boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <Box sx={{ width: 300, p: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row-reverse",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
           >
-            Clear
-          </Button>
-          <Button sx={{ width: "100%" }} color="warning" variant="contained">
-            Buy
-          </Button>
-        </Box>
+            <Typography sx={{ fontSize: 20 }}>Your Shopping Cart</Typography>
 
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            marginTop: 2,
-          }}
-        >
-          {cart.map((product) => (
-            <CartProduct key={product.id} product={product} />
-          ))}
+            <IconButton>
+              <Close onClick={closeDrawer} />
+            </IconButton>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              gap: 1,
+              mt: 2,
+            }}
+          >
+            <Button
+              onClick={clearCart}
+              sx={{ width: "100%" }}
+              color="warning"
+              variant="outlined"
+            >
+              Clear
+            </Button>
+            <Button sx={{ width: "100%" }} color="warning" variant="contained">
+              Buy
+            </Button>
+          </Box>
+          <Divider
+            sx={{
+              my: 2,
+            }}
+          />
+          <TotalSum />
         </Box>
-        <TotalSum />
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          marginTop: 2,
+        }}
+      >
+        {cart.map((product) => (
+          <CartProduct key={product.id} product={product} />
+        ))}
       </Box>
     </Drawer>
   );
--- src/components/TotalSum.jsx
+++ src/components/TotalSum.jsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import React from "react";
 import { useCart } from "../contexts/Cart";
 
@@ -7,5 +8,18 @@ export const TotalSum = () => {
     let sum = v.price * v.total;
     return p + sum;
   }, 0);
-  return <div>{summ.toFixed(2)}</div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        textAlign: "center",
+        padding: "5px",
+        background: (theme) => theme.palette.success.light,
+        color: "#fff",
+        borderRadius: "5px",
+      }}
+    >
+      Total sum: ${summ.toFixed(2)}
+    </Box>
+  );
 };
